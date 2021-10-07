c136b5cead132d4419d870e3249c4a23e5bdf7ce
Aurelius Dilectio Yuanata Djatmiko <aureliusdilectioyd@gmail.com>
2021-10-07 20:58:50 +0700

    Move circle with constant speed

---
+++ Assets/Scripts/CircleMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        MoveCircle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MoveCircle()
+    {
+        rb.AddForce(new Vector2(50, 15));
+    }
+
+}//class
