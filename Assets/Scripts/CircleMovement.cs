05eacc21db31f9e2cbb3d7d80b336d340f794031
Aurelius Dilectio Yuanata Djatmiko <aureliusdilectioyd@gmail.com>
2021-10-07 21:47:28 +0700

    Move circle with keyboard

--- Assets/Scripts/CircleMovement.cs
+++ Assets/Scripts/CircleMovement.cs
@@ -6,23 +6,48 @@ public class CircleMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
 
+    public float speed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
 
-        MoveCircle();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveCircle();
     }
 
     void MoveCircle()
     {
-        rb.AddForce(new Vector2(80, 15));
+        Vector2 velocity = rb.velocity;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            velocity.y = speed;
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            velocity.y = -speed;
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            velocity.x = speed;
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            velocity.x = -speed;
+        }
+        else
+        {
+            velocity.x = 0.0f;
+            velocity.y = 0.0f;
+        }
+
+        rb.velocity = velocity;
     }
 
 }//class
