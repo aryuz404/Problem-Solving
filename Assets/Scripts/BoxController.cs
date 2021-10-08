1a6f488c7d1a770d64b54a4cf24edb9655dbdcf1
Aurelius Dilectio Yuanata Djatmiko <aureliusdilectioyd@gmail.com>
2021-10-08 14:15:50 +0700

    Spawn random number of box in random position

---
+++ Assets/Scripts/BoxController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    public GameObject box;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnBox();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnBox()
+    {
+        int randomBox = Random.Range(1, 10);
+
+        for(int i = 0; i < randomBox; i++)
+        {
+            Vector3 boxPosition = new Vector3(Random.Range(-7f, 7f), Random.Range(-3f, 3f), 0f);
+            GameObject boxClone = Instantiate(box, boxPosition, Quaternion.identity);
+        }
+        
+    }
+
+}//class
